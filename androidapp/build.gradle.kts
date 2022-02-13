9c6426c39789b80f12166cd7e631d3f8ff3671a3
Ben Chatelain <ben@octop.ad>
2022-02-13 08:58:49 -0700

    🔧 Disable UnusedResources lint checks

--- androidapp/build.gradle.kts
+++ androidapp/build.gradle.kts
@@ -52,6 +52,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    lint {
+        checkDependencies = true
+        disable += "UnusedResources"
+    }
 }
 
 dependencies {
