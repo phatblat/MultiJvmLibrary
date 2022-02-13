c1168cc2a1ff080f9faa50738e7c015d9546c0f2
Ben Chatelain <ben@octop.ad>
2022-02-13 10:04:37 -0700

    🔧 Switch plugins to shorter syntax

--- androidapp/build.gradle.kts
+++ androidapp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
 }
 
 val appCompatVersion: String by project
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    `base`
+    base
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
+    kotlin("android") version "1.6.10" apply false
+    kotlin("jvm") version "1.6.10" apply false
 }
